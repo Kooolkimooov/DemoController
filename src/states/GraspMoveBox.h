317b976f599571d581e12eb2910fb1c580d5c05a
Martin Filliung <martin.filliung@aist.go.jp>
2026-03-03 11:40:09 +0900

    code format

--- src/states/GraspMoveBox.h
+++ src/states/GraspMoveBox.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <mc_control/fsm/State.h>
-#include <mc_tasks/TransformTask.h>
 #include <mc_tasks/RelativeEndEffectorTask.h>
+#include <mc_tasks/TransformTask.h>
 
 #include "mc_control/Contact.h"
 
@@ -48,29 +48,49 @@ struct GraspMoveBox : mc_control::fsm::State
             RemoveHands
         };
 
-        std::shared_ptr<mc_tasks::RelativeEndEffectorTask> m_leftGripperTask, m_rightGripperTask;
+        std::shared_ptr<mc_tasks::RelativeEndEffectorTask> m_leftGripperTask;
+        std::shared_ptr<mc_tasks::RelativeEndEffectorTask> m_rightGripperTask;
 
-        std::string m_objectName, m_objectSurfaceLeftGripper, m_objectSurfaceRightGripper;
+        std::string m_objectName;
+        std::string m_objectSurfaceLeftGripper;
+        std::string m_objectSurfaceRightGripper;
 
         Phase m_phase = Phase::None;
 
-        double m_stiffness = 1.0, m_weight = 1000.0, m_StartTime = 0.0, m_Timeout = 5.0, m_approachOffset = 0.0,
-               m_liftHeight = 0.1, m_liftDistance = 0.0, m_dropHeight = 0.1, m_dropDistance = 0.0,
-               m_completionEval = 0.05, m_completionSpeed = 1e-3, m_BoxHalfWidth = 0.0;
+        double m_stiffness          = 1.0;
+        double m_weight             = 1000.0;
+        double m_StartTime          = 0.0;
+        double m_Timeout            = 5.0;
+        double m_approachOffset     = 0.0;
+        double m_liftHeight         = 0.1;
+        double m_liftDistance       = 0.0;
+        double m_dropHeight         = 0.1;
+        double m_dropDistance       = 0.0;
+        double m_completionEval     = 0.05;
+        double m_completionSpeed    = 1e-3;
+        double m_BoxHalfWidth       = 0.0;
 
-        bool m_contactAdded = false, m_removeContactAtTeardown = true, m_manualPhaseChange = false,
-             m_allowPhaseChange = true;
+        bool m_contactAdded            = false;
+        bool m_removeContactAtTeardown = true;
+        bool m_manualPhaseChange       = false;
+        bool m_allowPhaseChange        = true;
 
-        mc_control::Contact m_leftContact{}, m_rightContact{};
+        mc_control::Contact m_leftContact{};
+        mc_control::Contact m_rightContact{};
 
-        Eigen::Vector3d m_graspFromPoseWorld, m_dropFromPoseWorld, m_Target2DRobot,
-                m_safeLeftHandPositionRobot  = Eigen::Vector3d(0.0, 0.25, 0.0),
-                m_safeRightHandPositionRobot = Eigen::Vector3d(0.0, -0.25, 0.0);
+        Eigen::Vector3d m_graspFromPoseWorld;
+        Eigen::Vector3d m_dropFromPoseWorld;
+        Eigen::Vector3d m_Target2DRobot;
+        Eigen::Vector3d m_safeLeftHandPositionRobot  = Eigen::Vector3d(0.0, 0.25, 0.0);
+        Eigen::Vector3d m_safeRightHandPositionRobot = Eigen::Vector3d(0.0, -0.25, 0.0);
 
-        Eigen::Quaterniond m_leftHandGraspOrientationRobot  = Eigen::Quaterniond(0.5, 0.5, 0.5, -0.5),
-                           m_rightHandGraspOrientationRobot = Eigen::Quaterniond(0.5, -0.5, 0.5, 0.5);
+        Eigen::Quaterniond m_leftHandGraspOrientationRobot  = Eigen::Quaterniond(0.5, 0.5, 0.5, -0.5);
+        Eigen::Quaterniond m_rightHandGraspOrientationRobot = Eigen::Quaterniond(0.5, -0.5, 0.5, 0.5);
 
-        sva::PTransformd m_leftHandTargetWorld, m_leftHandTargetRobot, m_rightHandTargetWorld, m_rightHandTargetRobot;
+        sva::PTransformd m_leftHandTargetWorld;
+        sva::PTransformd m_leftHandTargetRobot;
+        sva::PTransformd m_rightHandTargetWorld;
+        sva::PTransformd m_rightHandTargetRobot;
 
         Eigen::Matrix3d  toXYPlane(Eigen::Matrix3d);
         sva::PTransformd toHorizonAlignedPoseWorld(sva::PTransformd, sva::PTransformd);
